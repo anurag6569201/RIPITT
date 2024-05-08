b9411ce478216c6727874ac4ea96df282d144e9f
anurag singh <anurag6569201@gmail.com>
2024-05-09 02:35:07 +0530

    added full frontend

--- static/assets/js/custom.js
+++ static/assets/js/custom.js
@@ -53,7 +53,7 @@
                 dots: true,
                 items: 3,
                 margin: 30,
-                autoplay: false,
+                autoplay: true,
                 smartSpeed: 700,
                 autoplayTimeout: 6000,
                 responsive: {
@@ -76,36 +76,36 @@
                 }
             });
         }
-        if ($('.services-list').length) {
-            $('.services-list').owlCarousel({
-                loop: true,
-                nav: false,
-                dots: true,
-                items: 3,
-                margin: 30,
-                autoplay: false,
-                smartSpeed: 700,
-                autoplayTimeout: 6000,
-                responsive: {
-                    0: {
-                        items: 1,
-                        margin: 0
-                    },
-                    460: {
-                        items: 1,
-                        margin: 0
-                    },
-                    576: {
-                        items: 2,
-                        margin: 20
-                    },
-                    992: {
-                        items: 3,
-                        margin: 30
-                    }
-                }
-            });
-        }
+        // if ($('.services-list').length) {
+        //     $('.services-list').owlCarousel({
+        //         loop: true,
+        //         nav: false,
+        //         dots: true,
+        //         items: 3,
+        //         margin: 30,
+        //         autoplay: false,
+        //         smartSpeed: 700,
+        //         autoplayTimeout: 6000,
+        //         responsive: {
+        //             0: {
+        //                 items: 1,
+        //                 margin: 0
+        //             },
+        //             460: {
+        //                 items: 1,
+        //                 margin: 0
+        //             },
+        //             576: {
+        //                 items: 2,
+        //                 margin: 20
+        //             },
+        //             992: {
+        //                 items: 3,
+        //                 margin: 30
+        //             }
+        //         }
+        //     });
+        // }
         if ($('.gallery-list').length) {
             $('.gallery-list').owlCarousel({
                 loop: false,
