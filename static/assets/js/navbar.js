6c5ca9f6221f90f8bfc4030ce54aa6d38b167d2a
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-08-08 20:04:09 +0530

    adjusted for mobile

--- static/assets/js/navbar.js
+++ static/assets/js/navbar.js
@@ -11,14 +11,24 @@
       return window.attachEvent("onload", cb);
   }
 })(function () {
+    var app_wrapper=document.querySelector(".mobile-nav .app");
+    var app_full_wrapper=document.querySelector(".mobile-nav .wrapper");
   document.querySelectorAll('.nav-btn').forEach(function (el) {
       el.addEventListener('click', function () {
           var nav = document.getElementById('main-nav-back');
           var _class = 'open';
           if (nav.classList.contains(_class)) {
               nav.classList.remove(_class);
+              app_wrapper.style.height ="";
+              app_wrapper.style.width ="";
+              app_full_wrapper.style.height ="";
+              app_full_wrapper.style.width ="";
           } else {
               nav.classList.add(_class);
+              app_wrapper.style.height ="100vh";
+              app_wrapper.style.width ="100vw";
+              app_full_wrapper.style.height ="100vh";
+              app_full_wrapper.style.width ="100vw";
           }
       });
   });
