bba9a582a91b6b032f8fcf85f4c40cd202aa214a
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-08-12 15:59:18 +0530

    added new changes

--- static/assets/js/navbar.js
+++ static/assets/js/navbar.js
@@ -11,6 +11,7 @@
       return window.attachEvent("onload", cb);
   }
 })(function () {
+    var mobilenav_logo=document.querySelector(".mobile-nav-logo");
     var app_wrapper=document.querySelector(".mobile-nav .app");
     var app_full_wrapper=document.querySelector(".mobile-nav .wrapper");
   document.querySelectorAll('.nav-btn').forEach(function (el) {
@@ -19,12 +20,14 @@
           var _class = 'open';
           if (nav.classList.contains(_class)) {
               nav.classList.remove(_class);
+              mobilenav_logo.style.opacity ="0";
               app_wrapper.style.height ="";
               app_wrapper.style.width ="";
               app_full_wrapper.style.height ="";
               app_full_wrapper.style.width ="";
           } else {
               nav.classList.add(_class);
+              mobilenav_logo.style.opacity ="1";
               app_wrapper.style.height ="100vh";
               app_wrapper.style.width ="100vw";
               app_full_wrapper.style.height ="100vh";
