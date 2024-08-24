4d4b80fdae783ecf5c0913e26240ff7e780bc99c
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-08-24 09:16:20 +0530

    updated version2

--- version2/ripitt/urls.py
+++ version2/ripitt/urls.py
@@ -23,7 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('core.urls')),
-    path('',include('home.urls')),
 
     # ckeditor
     path("ckeditor5/", include('django_ckeditor_5.urls'), name="ck_editor_5_upload_file"),
